2155b5191f861a6eec0cddbb4916e3e17a3ccd6e
David Chambers <dc@davidchambers.me>
2017-02-19 17:32:00 +0100

    scripts: use sanctuary-def

--- scripts/eval.js
+++ scripts/eval.js
@@ -8,21 +8,38 @@ const Int = require('sanctuary-int');
 const Z = require('sanctuary-type-classes');
 
 
+//    Language :: Type
+const Language = $.EnumType('silly-goat/Language', '', ['javascript', 'text']);
+
+//    checkTypes :: Boolean
+const checkTypes = true;
+
+//    env :: Array Type
+const env = Z.concat(S.env, [Language]);
+
+//    def :: String -> StrMap TypeClass -> Array Type -> Function -> Function
+const def = $.create({checkTypes, env});
+
 //    evaluate :: String -> Either Error Any
-const evaluate = S.encaseEither3_(
-  S.I,
-  vm.runInNewContext,
-  S.__,
-  {$, Int, S, Z},
-  {timeout: 5000}
-);
+const evaluate =
+def('evaluate',
+    {},
+    [$.String, S.EitherType($.Error, $.Any)],
+    S.encaseEither3_(S.I,
+                     vm.runInNewContext,
+                     S.__,
+                     {$, Int, S, Z},
+                     {timeout: 5000}));
 
 //    backticks :: String
 const backticks = '```';
 
 //    formatCodeBlock :: String -> String -> String
 const formatCodeBlock =
-S.curry2((lang, code) => `${backticks}${lang}\n${code}\n${backticks}`);
+def('formatCodeBlock',
+    {},
+    [Language, $.String, $.String],
+    (lang, code) => `${backticks}${lang}\n${code}\n${backticks}`);
 
 
 module.exports = bot => {
