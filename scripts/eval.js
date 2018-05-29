dfb277ab45f760f0e327b6885dc133986f5b479e
David Chambers <dc@davidchambers.me>
2018-05-29 11:17:14 +0200

    create custom Sanctuary module to permit the use of Descending

--- scripts/eval.js
+++ scripts/eval.js
@@ -3,7 +3,7 @@
 const vm            = require('vm');
 
 const R             = require('ramda');
-const S             = require('sanctuary');
+const S_            = require('sanctuary');
 const $             = require('sanctuary-def');
 const Descending    = require('sanctuary-descending');
 const Int           = require('sanctuary-int');
@@ -14,18 +14,24 @@ const type          = require('sanctuary-type-identifiers');
 //    Language :: Type
 const Language = $.EnumType('silly-goat/Language', '', ['javascript', 'text']);
 
+//    env :: Array Type
+const env = S_.env.concat([
+  $.UnaryType
+    ('silly-goat/Descending')
+    ('https://github.com/sanctuary-js/sanctuary-descending')
+    (x => type(x) === 'sanctuary-descending/Descending@1')
+    (descending => [Z.extract(descending)])
+    ($.Unknown),
+]);
+
+//    opts :: { checkTypes :: Boolean, env :: Array Type }
+const opts = {checkTypes: true, env};
+
+//    S :: Module
+const S = S_.create(opts);
+
 //    def :: String -> StrMap TypeClass -> Array Type -> Function -> Function
-const def = $.create({
-  checkTypes: true,
-  env: S.env.concat([
-    $.UnaryType
-      ('silly-goat/Descending')
-      ('https://github.com/sanctuary-js/sanctuary-descending')
-      (x => type(x) === 'sanctuary-descending/Descending@1')
-      (descending => [Z.extract(descending)])
-      ($.Unknown),
-  ]),
-});
+const def = $.create(opts);
 
 //    evaluate :: String -> Either String String
 const evaluate =
